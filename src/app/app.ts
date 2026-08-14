27e81ab03e5871066cf6eb075a9f8a27cb1aee9b
agalvez-2022303 <agalvez-2022303@kinal.edu.gt>
2026-08-14 17:58:37 -0600

    feat: integrar navegacion y aplicar transiciones en AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,28 @@
 import { Component, signal } from '@angular/core';
+import { ShaderBackground } from './components/shader-background/shader-background';
+import { LandingView } from './views/landing-view/landing-view';
+import { LoginView } from './views/login-view/login-view';
+import { AlertModal } from './components/alert-modal/alert-modal';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ShaderBackground, LandingView, LoginView, AlertModal],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('money-tracker');
+  currentView = signal<'welcome' | 'login'>('welcome');
+  alertMessage = signal<string>('');
+
+  onNavigate(view: 'welcome' | 'login') {
+    this.currentView.set(view);
+  }
+
+  showAlert(msg: string) {
+    this.alertMessage.set(msg);
+  }
+
+  closeAlert() {
+    this.alertMessage.set('');
+  }
 }
